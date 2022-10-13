df9581c2271c6d31cc600bda07474826eb5250e8
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-10-13 18:49:39 +0530

    Add files via upload

---
+++ Phase 3 WebAPI CRUD Operations/SchoolWebAPI/School_BAL/BAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_BAL
+{
+    public class StudentsBAL
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int ClassID { get; set; }
+        public string SubjectName { get; set; }
+        public int? Marks { get; set; }
+    }
+}
---
+++ Phase 3 WebAPI CRUD Operations/SchoolWebAPI/School_Helper/Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using School_BAL;
+using School_DAL;
+
+namespace School_Helper
+{
+    public class StudentsHelper
+    {
+        StudentsDAL dal = null;
+        public StudentsHelper()
+        {
+            dal = new StudentsDAL();
+        }
+        public bool AddStudent(StudentsBAL s)
+        {
+            return dal.InsertStudent(s);
+        }
+        public bool EditStudent(StudentsBAL s)
+        {
+            return dal.UpdateStudent(s);
+        }
+        public bool RemoveStudent(int id)
+        {
+            return dal.DeleteStudent(id);
+        }
+        public StudentsBAL SearchStudent(int id)
+        {
+            return dal.FindStudent(id);
+        }
+        public void SearchStudent(int id, out StudentsBAL data)
+        {
+
+            dal.FindStudent(id, out data);
+        }
+        public int countStudent()
+        {
+            return dal.StudentCount();
+
+        }
+        public List<StudentsBAL> ShowStudentList()
+        {
+            return dal.StudentList();
+        }
+    }
+}
