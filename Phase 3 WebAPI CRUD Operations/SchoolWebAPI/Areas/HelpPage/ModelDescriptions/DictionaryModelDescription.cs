13520ca137e12df5dc8493635047c7419991c503
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-10-13 18:42:27 +0530

    Delete Phase 3 WebAPI CRUD Operations/SchoolWebAPI/Areas directory

--- Phase 3 WebAPI CRUD Operations/SchoolWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SchoolWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Phase 3 WebAPI CRUD Operations/SchoolWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SchoolWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
