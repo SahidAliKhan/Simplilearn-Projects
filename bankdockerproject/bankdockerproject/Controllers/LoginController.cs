2e81afe864b9212ce9d1e5791789259a19fc44c3
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-11-01 17:57:51 +0530

    Add files via upload

---
+++ bankdockerproject/bankdockerproject/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using bankdockerproject.Models;
+
+namespace bankdockerproject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<LoginModel> value()
+        {
+            var users = new List<LoginModel>
+            {
+                new LoginModel{username="sahidxb",password="Hello@123"},
+                new LoginModel{username="sahid",password="Hello@123"},
+            };
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(LoginModel users)
+        {
+            var u = value();
+
+            var ue = u.Where(u => u.username.Equals(users.username));
+
+            var up = ue.Where(p => p.password.Equals(users.password));
+
+            if (up.Count() == 1)
+            {
+                ViewBag.message = "Login Success";
+                return View("Dashboard");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ bankdockerproject/bankdockerproject/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bankdockerproject.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
